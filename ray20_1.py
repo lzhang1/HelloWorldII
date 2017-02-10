f6c8f4ce7b5ac05c946d69cfe7cbe1db0d58256b
leizhang <lzhang@redhat.com>
2017-02-10 15:45:31 +0800

    update  pyqt5

--- ray20_1.py
+++ ray20_1.py
@@ -1,17 +1,48 @@
 # -*- coding: UTF-8 -*-
 
-from PyQt5 import QtWidgets
-from uiform import Ui_MainWindow
+# from PyQt5 import QtWidgets
+# from first import Ui_MainWindow
+#
+# class myWindow(QtWidgets.QWidget, Ui_MainWindow):
+#     def __init__(self):
+#         super(myWindow, self).__init__()
+#         self.new = Ui_MainWindow()
+#         self.new.setupUi(self)
+#
+# if __name__=="__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     myshow = myWindow()
+#     myshow.show()
+#     sys.exit(app.exec_())
 
-class myWindow(QtWidgets.QWidget):
-    def __init__(self):
-        super(myWindow, self).__init__()
-        self.new = Ui_MainWindow()
-        self.new.setupUi(self)
+# import sys
+#
+# from PyQt5.QtWidgets import QApplication , QMainWindow
+#
+# from firstPyQt5 import *
+#
+# if __name__ == '__main__':
+#     '''
+#     主函数
+#     '''
+
+    # app = QApplication(sys.argv)
+    # mainWindow = QMainWindow()
+    # ui = Ui_MainWindow()
+    # ui.setupUi(mainWindow)
+    # mainWindow.show()
+    # sys.exit(app.exec_())
+
+from PyQt5 import *
+from firstPyQt5 import *
+import sys
 
 if __name__=="__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    myshow = myWindow()
-    myshow.show()
+    app=QtWidgets.QApplication(sys.argv)
+    widget=QtWidgets.QWidget()
+    ui=Ui_MainWindow()
+    ui.setupUi(widget)
+    widget.show()
     sys.exit(app.exec_())
